b1046c29b7991e73b61996d232b616ec87d0cf98
Bartosz Leśniewski <b.lesniewski8@gmail.com>
2021-07-19 14:52:36 +0200

    added random riddle and category information

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -1,4 +1,22 @@
 class Game {
+
+    quotes = [{
+        title: 'y tu mama tambien',
+        category: 'film'
+    }, {
+        title: 'z szynka raz',
+        category: 'książka'
+    }, {
+        title: 'everybodys gotta live',
+        category: 'piosenka'
+    }, {
+        title: 'a ci, ktorzy tanczyli, zostali uznani za szalonych przez tych, ktorzy nie słyszeli muzyki',
+        category: 'cytat'
+    }, {
+        title: 'tomasz michniewicz',
+        category: 'pisarz'
+    }]
+
     constructor({
         outputWrapper,
         wordWrapper,
@@ -11,7 +29,14 @@ class Game {
         this.lettersWrapper = lettersWrapper;
     }
 
-    start() {
+    initQuote() {
+        const quote = this.quotes[Math.floor(Math.random() * this.quotes.length)]
+        this.wordWrapper.innerHTML = quote.title
+        this.categoryWrapper.innerHTML = quote.category
+        console.log(quote)
+    }
+
+    initLetters() {
         for (let i = 0; i < 26; i++) {
             const letter = (i + 10).toString(36)
             const btnLetter = document.createElement('button')
@@ -21,6 +46,11 @@ class Game {
                 console.log(letter)
             })
         }
+    }
+
+    start() {
+        this.initLetters()
+        this.initQuote()
     }
 }
 
