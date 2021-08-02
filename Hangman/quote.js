aa5644508ada28aea7b54365b6783be6344d7d48
Bartosz Leśniewski <b.lesniewski8@gmail.com>
2021-08-02 21:20:21 +0200

    html rewrite

--- Hangman/quote.js
+++ Hangman/quote.js
@@ -1,26 +1,26 @@
-export class Quote {
-  constructor(text) {
-    this.text = text;
-    this.answered = [];
-  }
+// export class Quote {
+//   constructor(text) {
+//     this.text = text;
+//     this.answered = [];
+//   }
 
-  getText() {
-    let content = '';
-    for (const letter of this.text) {
-      if (letter == ' ' || this.answered.includes(letter)) {
-        content += letter;
-      } else {
-        content += '_';
-      }
-    }
-    return content;
-  }
+//   getText() {
+//     let content = '';
+//     for (const letter of this.text) {
+//       if (letter == ' ' || this.answered.includes(letter)) {
+//         content += letter;
+//       } else {
+//         content += '_';
+//       }
+//     }
+//     return content;
+//   }
 
-  answer(letter) {
-    if (!this.text.includes(letter)) {
-      return false;
-    }
-    this.answered.push(letter);
-    return true;
-  }
-}
+//   answer(letter) {
+//     if (!this.text.includes(letter)) {
+//       return false;
+//     }
+//     this.answered.push(letter);
+//     return true;
+//   }
+// }
