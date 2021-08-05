bcc7e797a38c23f3a22c2fd57ece3ed657465eea
Bartosz Leśniewski <b.lesniewski8@gmail.com>
2021-08-05 15:34:35 +0200

    info about game updated in main.js

--- main.js
+++ main.js
@@ -8,21 +8,21 @@ const content = [
     image:
       '<a href="https://bartosz-lesniewski.github.io/games/Hangman/hangman.html"><img  class=\'bottom__img\' src="https://bartosz-lesniewski.github.io/games/Hangman/images/okladka.png" alt="Game picture"></a>',
     gameInfo:
-      "<h2 class='top__h2'>- Kliknij w obrazek aby włączyć grę</br>- Early access nie bierze pod uwagę znaków diaktrycznych, weź to pod uwagę :)</br>- Masz 8 żyć</br></h2><h3>Baw się dobrze :)</h3>",
+      "<h2 class='top__h2'>- Click on the image to start play,</br>- Game is version 1.0.0 - so its completed :)</br>- You have 8 lives</br></h2><h3>Have a fun! :)</h3>",
   },
   {
     game: 'Saper',
     image:
       '<a href="https://bartosz-lesniewski.github.io/games/Saper/saper.html"><img class=\'bottom__img\' src="https://bartosz-lesniewski.github.io/games/Saper/images/okladka.png" alt="Game picture"></a>',
     gameInfo:
-      "<h2 class='top__h2'>- Kliknij w obrazek aby włączyć grę</br>- gra w wersji Alpha, jak trafisz na minę, to gra zwróci czerwone tło w tym miejscu. </br>- Można grać, wygrać lub przegrać, ale obecna wersja nie podpowiada ile min jest przy danym polu</br></h2><h3>Baw się dobrze :)</h3>",
+      "<h2 class='top__h2'>- Click on the image to start play,</br>- game is alpha version, you can flag (orange), find field without mine (green) or find mine (red)</br>- mines is random every time</br></h2><h3>Have a fun! :)</h3>",
   },
   {
     game: 'One Armed Bandit',
     image:
       '<a href="https://bartosz-lesniewski.github.io/games/One Armed Bandit/index.html"><img class=\'bottom__img\' src="https://bartosz-lesniewski.github.io/games/One Armed Bandit/okladka.png" alt="Game picture"></a>',
     gameInfo:
-      "<h2 class='top__h2'>- Kliknij w obrazek aby włączyć grę</br>- gra w wersji Beta. </br>- Można grać, wygrać lub przegrać, obecna wersja nie podlicza statystyk, reszta działa jak należy :)</br></h2><h3>Baw się dobrze :)</h3>",
+      "<h2 class='top__h2'>- Click on the image to start play,</br>- game is in beta </br>- You can play, win money or loose all of it.</br></h2><h3>Have a fun! :)</h3>",
   },
 ];
 
